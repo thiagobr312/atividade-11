10c7fa89ef84c82d34e2fe47474498745f923167
thiagobr312 <yurithiago-@hotmail.com>
2018-09-22 12:30:38 -0300

    subindo atividade-11

---
+++ java script/java.js
@@ -0,0 +1,9 @@
+function show(){
+    document.getElementById("text").style.display="block"
+}
+function ocult(){
+    document.getElementById("text").style.display="none"
+}
+function change(){
+    document.getElementById("text").style.color="silver"
+}
